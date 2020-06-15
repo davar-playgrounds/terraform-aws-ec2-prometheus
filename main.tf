d5562e4790c1ed81356e3b11150836e24e872dfd
haitham <h.ghalwash@gmail.com>
2020-06-15 17:42:58 -0400

    Monitor-infra-v1

--- main.tf
+++ main.tf
@@ -50,11 +50,11 @@ Check out all the available sub-modules at:
 https://github.com/terraform-aws-modules/terraform-aws-security-group/tree/master/modules
  */
 
-module "ssh_sg" {
+module "prometheus_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "3.3.0"
 
-  name        = "ssh_sg"
+  name        = "prometheus_sg"
   description = "Security group for instances"
   vpc_id      = "${module.sandbox_vpc.vpc_id}"
   
@@ -82,18 +82,49 @@ module "ssh_sg" {
     Terraform   = "true"
   }
 }
-resource "tls_private_key" "sskeygen_execution" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+# Configuration for a "master" instance
+
+resource "aws_instance" "monitor_hub" {
+  ami           = var.amis[var.aws_region]
+  instance_type = var.instance_type
+  key_name      = var.keypair_name
+  # tags = module.label.tags
+  vpc_security_group_ids      = [module.prometheus_sg.this_security_group_id]
+  subnet_id                   = module.sandbox_vpc.public_subnets[0]
+  associate_public_ip_address = true
+  root_block_device {
+    volume_size = var.root_volume_size
+  }
+  tags = {
+    Name        = "monitor-hub"
+    Owner       = var.fellow_name
+    Environment = "dev"
+    Terraform   = "true"
+  }
+  connection {
+    user        = "ubuntu"
+    host = self.public_ip
+    private_key = "${file("/home/haitham/.ssh/Haitham-IAM-keypair.pem")}"
+    # private_key = file(var.keypair_name)
+  }
+# # Install docker in the ubuntu
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo apt update",
+  #     "sudo apt install -y python3-pip",
+  #     "sudo pip3 install prometheus_client"
+  #     "git clone https://github.com/ghalwash/icon-prometheus-exporter",
+  #     "cd icon_prometheus_exporter/icon_prometheus_exporter/"
+  #   ]
+  # }
 }
 
-# Configuration for a "master" instance
 resource "aws_instance" "Prometheus_master" {
   ami           = var.amis[var.aws_region]
   instance_type = var.instance_type
   key_name      = var.keypair_name
   # tags = module.label.tags
-  vpc_security_group_ids      = [module.ssh_sg.this_security_group_id]
+  vpc_security_group_ids      = [module.prometheus_sg.this_security_group_id]
   subnet_id                   = module.sandbox_vpc.public_subnets[0]
   associate_public_ip_address = true
     root_block_device {
@@ -105,15 +136,14 @@ resource "aws_instance" "Prometheus_master" {
     Environment = "dev"
     Terraform   = "true"
   }
-#   iam_instance_profile = aws_iam_instance_profile.this.id
-  # key_name             = var.public_key_path == "" ? var.key_name : aws_key_pair.this.*.key_name[0]
-connection {
-    user     = "ubuntu"
+  connection {
+    user        = "ubuntu"
     host = self.public_ip
-    private_key = tls_private_key.sskeygen_execution.private_key_pem
+    private_key = "${file("/home/haitham/.ssh/Haitham-IAM-keypair.pem")}"
+    # private_key = file(var.keypair_name)
   }
-
-provisioner "remote-exec" {
+# # Install docker in the ubuntu
+  provisioner "remote-exec" {
     inline = [
       "sudo apt update",
       "sudo apt -y install apt-transport-https ca-certificates curl software-properties-common",
@@ -121,13 +151,12 @@ provisioner "remote-exec" {
       "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'",
       "sudo apt update",
       "sudo apt -y install docker-ce",
+      "sudo apt  -y install docker-compose",
       "git clone https://github.com/stefanprodan/dockprom",
-      "cd dockprom",
-      "ADMIN_USER=admin ADMIN_PASSWORD=admin docker-compose up -d"
+      "pwd",
+      "cd dockprom/",
+      "sudo ADMIN_USER=admin ADMIN_PASSWORD=admin docker-compose up -d"
     ]
-  }
-provisioner "local-exec" {
-    command = "echo '${tls_private_key.sskeygen_execution.private_key_pem}'"
   }
 }
 # # Configuration for an Elastic IP to add to nodes
@@ -135,8 +164,8 @@ resource "aws_eip" "elastic_ips_for_instances" {
   vpc = true
   instance = aws_instance.Prometheus_master.id
   tags = {
-    NetworkName = var.network_name
-    Owner       = var.owner
+    NetworkName = "main"
+    Owner       = "Haitham"
     Terraform   = true
     VpcType     = "main"
   }
@@ -146,14 +175,15 @@ resource "aws_eip_association" "elastic_ips_association" {
   instance_id = aws_instance.Prometheus_master.id
   public_ip   = aws_eip.elastic_ips_for_instances.public_ip
 }
+
 # module "ansible" {
 #   source           = "github.com/insight-infrastructure/terraform-aws-ansible-playbook.git?ref=master"
-#   ip               = aws_eip_association.this.public_ip
+#   ip               = aws_eip_association.elastic_ips_association.public_ip
 #   user             = "ubuntu"
 #   private_key_path = var.private_key_path
 #   playbook_file_path = "${path.module}/ansible/main.yml"
 #   playbook_vars = {
-#     ansible_host = var.root_domain_name == "" ? aws_eip.this.public_ip : join("", aws_route53_record.this.*.fqdn)
+#     ansible_host = var.root_domain_name == "" ? aws_eip.elastic_ips_for_instances.public_ip : join("", aws_route53_record.www.*.fqdn)
 #   }
 #   playbook_vars_file = var.playbook_vars_file
 #   requirements_file_path = "${path.module}/ansible/requirements.yml"
@@ -172,7 +202,7 @@ resource "aws_eip_association" "elastic_ips_association" {
 # resource "aws_volume_attachment" "this" {
 #   count = var.ebs_volume_size > 0 ? 1 : 0
 #   device_name = var.volume_path
-#   volume_id = aws_ebs_volume.this.*.id[0]
+# volume_id = aws_ebs_volume.this.*.id[0]
 #   instance_id  = aws_instance.this.id
 #   force_detach = true
 # }
@@ -235,7 +265,7 @@ Note: You don't need the below resources for using the Pegasus tool
 # resource "aws_route53_record" "www" {
 #   # count = var.root_domain_name == "" ? 0 : 1
 #   zone_id = data.aws_route53_zone.selected.zone_id
-#   name = ".${data.aws_route53_zone.selected.name}"
+#   name = "www.${data.aws_route53_zone.selected.name}"
 #   type = "A"
 #   ttl  = "300"
 #   records = ["${aws_eip.elastic_ips_for_instances.public_ip}"]
--- variables.tf
+++ variables.tf
@@ -15,90 +15,9 @@ data "aws_availability_zones" "available" {
     state = "available"
 }
 
-########
-# Label
-########
-variable "name" {
-  description = "The name for the label"
-  type        = string
-  default     = "prometheus"
-}
-
-variable "environment" {
-  description = "The environment"
-  type        = string
-  default     = ""
-}
-
-variable "namespace" {
-  description = "The namespace to deploy into"
-  type        = string
-  default     = "prod"
-}
-
-variable "stage" {
-  description = "The stage of the deployment"
-  type        = string
-  default     = "blue"
-}
-
-variable "network_name" {
-  description = "The network name, ie kusama / mainnet"
-  type        = string
-  default     = "main"
-}
-
-variable "owner" {
-  description = "Owner of the infrastructure"
-  type        = string
-  default     = ""
-}
-
 ######
 # Data
 ######
-variable "eip_id" {
-  description = "The elastic ip id to attach to active instance"
-  type        = string
-  default     = ""
-}
-
-# variable "subnet_id" {
-#   description = "The id of the subnet"
-#   type        = string
-# }
-
-# variable "vpc_security_group_ids" {
-#   description = "List of security groups"
-#   type        = list(string)
-# }
-
-#####
-# ec2
-#####
-variable "key_name" {
-  description = "The key pair to import"
-  type        = string
-  default     = ""
-}
-
-variable "monitoring" {
-  description = "Boolean for cloudwatch"
-  type        = bool
-  default     = false
-}
-
-//variable "ebs_volume_size" {
-//  description = "EBS volume size"
-//  type        = string
-//  default     = 0
-//}
-
-//variable "volume_path" {
-//  description = "Volume path"
-//  type        = string
-//  default     = "/dev/xvdf"
-//}
 variable "keypair_name" {
   description = "Haitham-IAM-keypair"
 }
@@ -108,125 +27,18 @@ variable "amis" {
     "us-east-1" = "ami-0b6b1f8f449568786"
   }
 }
-
 variable "cluster_name" {
   description = "Prometheus cluster"
   default     = "Prometheus"
 }
-
-
 variable "root_volume_size" {
   description = "Root volume size"
   type        = string
   default     = 100
 }
-
 variable "instance_type" {
   description = "Instance type"
   type        = string
   default     = "m4.large"
   #"t2.small"
 }
-
-variable "public_key_path" {
-  description = "The path to the public ssh key"
-  type        = string
-}
-
-variable "private_key_path" {
-  description = "The path to the private ssh key"
-  type        = string
-}
-
-#####
-# EBS
-#####
-variable "ebs_volume_id" {
-  type        = string
-  default     = ""
-  description = "The volume id of the ebs volume to mount"
-}
-
-variable "ebs_volume_size" {
-  description = "The size of volume - leave as zero or empty for no volume"
-  type        = number
-  default     = 0
-}
-
-variable "volume_path" {
-  description = "The path of the EBS volume"
-  type        = string
-  default     = "/dev/xvdf"
-}
-
-
-#########
-# Ansible
-#########
-variable "playbook_vars" {
-  description = "Extra playbook vars"
-  type        = map(string)
-  default     = {}
-}
-
-variable "playbook_vars_file" {
-  description = "Path to vars file"
-  type        = string
-  default     = ""
-}
-
-variable "alertmanager_receivers" {
-  description = "list of recievers per docs https://prometheus.io/docs/alerting/configuration/#example"
-  type        = list(map(string))
-  default     = [{}]
-}
-
-
-//variable "private_key_path" {
-//  description = "Path to the private ssh key"
-//  type        = string
-//  default     = ""
-//}
-//
-//variable "playbook_file_path" {
-//  description = "The path to the playbook"
-//  type        = string
-//  default     = ""
-//}
-//
-//variable "user" {
-//  description = "The user for configuring node with ansible"
-//  type        = string
-//  default     = "ubuntu"
-//}
-variable "root_domain_name" {
-  description = "The name of the root domain name"
-  type        = string
-  default     = "netdevops.tech/"
-}
-variable "hostname" {
-  description = "Boolean to create a hostname - ie hostname.example.com"
-  type        = string
-  default     = ""
-}
-/*	
-
-Not using AWS credential variables since they're automatically detected 
-from the environment variables
-
-However, you could remove this and use a **properly** secured variable file
-If you prefer to use a variable file, then you should NOT commit that file 
-and should use a proper security measures (e.g. use .gitignore, restrict access)
-
-It is also worth noting that Terraform stores state in plaintext, so should
-be used in production with a remote backend that is encrypted (e.g. S3, Consul)
-
-variable "aws_access_key" {
-	description = "AWS access key (e.g. ABCDE1F2G3HIJKLMNOP )"	
-}
-
-variable "aws_secret_key" {
-	description = "AWS secret key (e.g. 1abc2d34e/f5ghJKlmnopqSr678stUV/WXYZa12 )"	
-}
-
- */
